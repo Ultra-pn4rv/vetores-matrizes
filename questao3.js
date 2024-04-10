d8c7ceaf47d26b62b5c61efd393ae0eaabff2a2a
Ultra-pn4rv <tsarultra@gmail.com>
2024-04-10 11:21:16 -0400

    questao 3

---
+++ questao3.js
@@ -0,0 +1,8 @@
+/*
+Questão 3: Crie um vetor com 10 elementos e remova o último elemento e imprima
+o vetor sem o elemento removido em JavaScript?
+*/
+let meuArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let elementoRemove = meuArray.pop();
+
+console.log(meuArray);
