6c220b7e7041fc4506f4ccfee53c0e736a5c4300
Ultra-pn4rv <tsarultra@gmail.com>
2024-04-10 11:20:46 -0400

    questao 1

---
+++ questao1.js
@@ -0,0 +1,13 @@
+/*
+Questão 1: Crie um vetor em JavaScript contendo os números de 1 a 5 e imprima-o
+na tela.
+*/
+let meuArray = [];
+
+meuArray.push(1);
+meuArray.push(2);
+meuArray.push(3);
+meuArray.push(4);
+meuArray.push(5);
+
+console.log(meuArray);
