6292b5bf09c5b423465fbb969681491d6cb37835
Ultra-pn4rv <tsarultra@gmail.com>
2024-04-10 11:21:51 -0400

    questao 7

---
+++ questao7.js
@@ -0,0 +1,9 @@
+/*
+Questão 7: Crie uma matriz 3x3 e escreva uma função para verificar se a matriz é
+simétrica em relação à sua diagonal principal.
+*/
+let matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
